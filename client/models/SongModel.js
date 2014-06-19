365a9700d95c367be8f0dd3033c129a94ab6e774
taojonesin <taojonesin (at) nowhere.com>
2014-06-18 22:11:17 -0700

    added incPlayCount function to SongModel, increment on play event

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -3,8 +3,16 @@ var SongModel = Backbone.Model.extend({
   initialize: function() {
     this.set({playCount: 0});
   },
+  playCount: function() {
+    return this.get('playCount');
+  },
+  incPlayCount: function(){
+    //write a function that will set the 'votes' to the current votecount plus one.  Stuck?  check out http://backbonejs.org/#Model-set
+    this.set({playCount: this.playCount() + 1});
+  },
   play: function(){
     // Triggering an event here will also trigger the event on the collection
+    this.incPlayCount();
     this.trigger('play', this);
   }
 
