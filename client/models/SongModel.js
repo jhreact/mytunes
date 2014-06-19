9ead2bdc66963da1414516630a290bd9b0045986
taojonesin <taojonesin (at) nowhere.com>
2014-06-19 03:10:21 -0700

    SongModel: added enqueue trigger

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -3,6 +3,14 @@ var SongModel = Backbone.Model.extend({
   initialize: function() {
     this.set({playCount: 0});
   },
+  play: function(){
+    // Triggering an event here will also trigger the event on the collection
+    this.incPlayCount();
+    this.trigger('play', this);
+  },
+  enqueue: function() {
+    this.trigger('enqueue', this);
+  },
   playCount: function() {
     return this.get('playCount');
   },
@@ -10,10 +18,6 @@ var SongModel = Backbone.Model.extend({
     //write a function that will set the 'votes' to the current votecount plus one.  Stuck?  check out http://backbonejs.org/#Model-set
     this.set({playCount: this.playCount() + 1});
   },
-  play: function(){
-    // Triggering an event here will also trigger the event on the collection
-    this.incPlayCount();
-    this.trigger('play', this);
-  }
+
 
 });
