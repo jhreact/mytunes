ab9c6595abfdda8ba769573bcd6e4a65914b6bdf
taojonesin <taojonesin (at) nowhere.com>
2014-06-18 22:22:40 -0700

    library view re-renders on playCount change

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -1,6 +1,12 @@
 // LibraryEntryView.js - Defines a backbone view class for the entries that will appear within the library views. These will be inserted using the "subview" pattern.
 var LibraryEntryView = Backbone.View.extend({
 
+  initialize: function() {
+    this.model.on('change:playCount', function(){
+      this.render();
+    }, this);
+  },
+
   tagName: 'tr',
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td><%= playCount %></td>'),
