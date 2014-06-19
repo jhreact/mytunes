2a509d833d8261abc1bd21eca28f396feb31d2a3
taojonesin <taojonesin (at) nowhere.com>
2014-06-19 11:11:14 -0700

    SongQueueEntryView: rendering, event listening, etc

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,24 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+  initialize: function() {
+    // Listen for model changes and propagate to ui
+    this.model.on('change', this.render, this);
+  },
+
+  className: 'songQueueEntryViewRow',
+  tagName: 'tr',
+
+  template: _.template('<td class="songQueueEntryTitle"><%= title %></td><td class="songQueueEntryArtist"><%= artist %></td>'),
+
+  // Listen for ui events and propagate to model
+  events: {
+    'click': function() {
+      this.model.enqueue();
+    }
+  },
+
+  render: function() {
+    return this.$el.html(this.template(this.model.toJSON()));
+  }
+
 });
